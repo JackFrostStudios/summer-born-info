98f743af1437d9df3b82fe2af0ec9ca268c55353
JackFrostStudios <jack_reeves@live.com>
2024-12-06 16:58:47 +0000

    Add LocalAuthority FindById

---
+++ SummerBornInfo.Web/Features/LocalAuthorityQueries/FindById/Endpoint.cs
@@ -0,0 +1,26 @@
+﻿namespace SummerBornInfo.Web.Features.LocalAuthorityQueries.FindById;
+
+internal sealed class Endpoint(SchoolContext context) : Endpoint<Request, Response, Mapper>
+{
+    private readonly SchoolContext context = context;
+    public override void Configure()
+    {
+        AllowAnonymous();
+        Get("local-authority/{id}");
+        Description(g => g.ProducesProblem(404));
+    }
+
+    public override async Task HandleAsync(Request request, CancellationToken cancellation)
+    {
+        var authority = await context.LocalAuthority.AsNoTracking().FirstOrDefaultAsync(la => la.Id == request.Id, cancellationToken: cancellation);
+        if (authority == null)
+        {
+            await SendNotFoundAsync(cancellation);
+        }
+        else
+        {
+            await SendAsync(response: Map.FromEntity(authority), cancellation: cancellation);
+            return;
+        }
+    }
+}
