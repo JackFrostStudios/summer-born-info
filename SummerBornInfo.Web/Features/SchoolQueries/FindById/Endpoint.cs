faa4ae2eeee6f113301244aa5ac34d41e4ca6e01
JackFrostStudios <jack_reeves@live.com>
2024-12-06 17:22:23 +0000

    Add School FindById

---
+++ SummerBornInfo.Web/Features/SchoolQueries/FindById/Endpoint.cs
@@ -0,0 +1,26 @@
+﻿namespace SummerBornInfo.Web.Features.SchoolQueries.FindById;
+
+internal sealed class Endpoint(SchoolContext context) : Endpoint<Request, Response, Mapper>
+{
+    private readonly SchoolContext context = context;
+    public override void Configure()
+    {
+        AllowAnonymous();
+        Get("school/{id}");
+        Description(g => g.ProducesProblem(404));
+    }
+
+    public override async Task HandleAsync(Request request, CancellationToken cancellation)
+    {
+        var school = await context.School.AsNoTracking().FirstOrDefaultAsync(s => s.Id == request.Id, cancellationToken: cancellation);
+        if (school == null)
+        {
+            await SendNotFoundAsync(cancellation);
+        }
+        else
+        {
+            await SendAsync(response: Map.FromEntity(school), cancellation: cancellation);
+            return;
+        }
+    }
+}
---
+++ SummerBornInfo.Web/Features/SchoolQueries/FindById/Models.cs
@@ -0,0 +1,49 @@
+﻿namespace SummerBornInfo.Web.Features.SchoolQueries.FindById;
+
+internal sealed class Request
+{
+    public Guid Id { get; set; }
+
+    internal sealed class Validator : Validator<Request>
+    {
+        public Validator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty();
+        }
+    }
+}
+
+internal sealed class Response
+{
+    public required Guid Id { get; init; }
+    public required int URN { get; init; }
+    public required int UKPRN { get; init; }
+    public required int EstablishmentNumber { get; init; }
+    public required string Name { get; init; }
+    public required AddressResponse Address { get; init; }
+    public required DateOnly OpenDate { get; init; }
+    public DateOnly CloseDate { get; init; }
+    public required CodeNameEntityResponse PhaseOfEducation { get; init; }
+    public required CodeNameEntityResponse LocalAuthority { get; init; }
+    public required CodeNameEntityResponse EstablishmentType { get; init; }
+    public required CodeNameEntityResponse EstablishmentGroup { get; init; }
+    public required CodeNameEntityResponse EstablishmentStatus { get; init; }
+
+    internal sealed class AddressResponse
+    {
+        public string? Street { get; init; }
+        public string? Locality { get; init; }
+        public string? AddressThree { get; init; }
+        public string? Town { get; init; }
+        public string? County { get; init; }
+        public string? PostCode { get; init; }
+    }
+
+    internal sealed class CodeNameEntityResponse
+    {
+        public required Guid Id { get; init; }
+        public required int Code { get; init; }
+        public required string Name { get; init; }
+    }
+}
