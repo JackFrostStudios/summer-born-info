5ac3e295680122666e341ee52a57b30add742230
JackFrostStudios <jack_reeves@live.com>
2024-12-06 16:53:53 +0000

    Add EstablishmentType FindById

---
+++ SummerBornInfo.Web/Features/EstablishmentTypeQueries/FindById/Mapper.cs
@@ -0,0 +1,11 @@
+﻿namespace SummerBornInfo.Web.Features.EstablishmentTypeQueries.FindById;
+
+internal sealed class Mapper : ResponseMapper<Response, EstablishmentType>
+{
+    public override Response FromEntity(EstablishmentType e) => new()
+    {
+        Id = e.Id,
+        Code = e.Code,
+        Name = e.Name
+    };
+}
