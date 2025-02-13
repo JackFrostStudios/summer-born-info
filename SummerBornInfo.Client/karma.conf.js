e4facfda02965ca3f77fa9251596a4b6be1c0bfe
JackFrostStudios <jack_reeves@live.com>
2025-02-13 22:12:18 +0000

    Add separate client test step and generate the required reports

--- SummerBornInfo.Client/karma.conf.js
+++ SummerBornInfo.Client/karma.conf.js
@@ -7,7 +7,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-trx-reporter')
     ],
     client: {
       jasmine: {
@@ -20,6 +21,10 @@ module.exports = function (config) {
     },
     jasmineHtmlReporter: {
       suppressAll: true // removes the duplicated traces
+    },
+    trxReporter: {
+      outputFile: 'SummerBornInfo.Client.trx',
+      shortTestName: false
     },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/'),
