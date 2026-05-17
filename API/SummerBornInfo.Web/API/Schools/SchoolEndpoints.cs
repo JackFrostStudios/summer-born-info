97049e061379c7167df9773e7fa4c33ff13d24d5
JackFrostStudios <jack_reeves@live.com>
2026-05-17 21:13:02 +0100

    Refactor school endpoints to be more maintainable.

--- API/SummerBornInfo.Web/API/Schools/SchoolEndpoints.cs
+++ API/SummerBornInfo.Web/API/Schools/SchoolEndpoints.cs
@@ -6,7 +6,26 @@ public static class SchoolEndpoints
     {
         var schools = endpoints.MapGroup("/api/schools");
 
-        _ = schools.MapPost("/import", async (HttpRequest req, Stream csvFile, ImportSchoolsCommandHandler handler, CancellationToken cancellationToken) =>
+        _ = schools.MapGetAllSchools()
+            .MapCreateSchoolsImportRequest()
+            .MapGetSchoolsImportRequest();
+    }
+
+    private static RouteGroupBuilder MapGetAllSchools(this RouteGroupBuilder builder)
+    {
+        _ = builder.MapGet("/", async (GetAllSchoolsQueryHandler handler, Guid? cursor, int? pageSize, CancellationToken cancellationToken) =>
+        {
+            GetAllSchoolsQuery query = new(cursor, pageSize ?? 100);
+            (var schools, var nextCursor) = await handler.ExecuteAsync(query, cancellationToken);
+            return Results.Ok(new { schools, nextCursor });
+        });
+
+        return builder;
+    }
+
+    private static RouteGroupBuilder MapCreateSchoolsImportRequest(this RouteGroupBuilder builder)
+    {
+        _ = builder.MapPost("/import", async (HttpRequest req, Stream csvFile, ImportSchoolsCommandHandler handler, CancellationToken cancellationToken) =>
         {
             var maxMessageSize = 100000 * 1024;
             if (req.ContentLength is not null && req.ContentLength > maxMessageSize)
@@ -23,15 +42,12 @@ public static class SchoolEndpoints
             return Results.Ok(result);
         })
             .WithMetadata(new RequestSizeLimitAttribute(100000 * 1024));
+        return builder;
+    }
 
-        _ = schools.MapGet("/", async (GetAllSchoolsQueryHandler handler, Guid? cursor, int? pageSize, CancellationToken cancellationToken) =>
-        {
-            GetAllSchoolsQuery query = new(cursor, pageSize ?? 100);
-            (var schools, var nextCursor) = await handler.ExecuteAsync(query, cancellationToken);
-            return Results.Ok(new { schools, nextCursor });
-        });
-
-        _ = schools.MapGet("/import/{requestId:guid}", async (GetSchoolBulkImportStatusQueryHandler handler, Guid requestId, CancellationToken cancellationToken) =>
+    private static RouteGroupBuilder MapGetSchoolsImportRequest(this RouteGroupBuilder builder)
+    {
+        _ = builder.MapGet("/import/{requestId:guid}", async (GetSchoolBulkImportStatusQueryHandler handler, Guid requestId, CancellationToken cancellationToken) =>
         {
             GetSchoolBulkImportStatusQuery query = new(requestId);
             var result = await handler.ExecuteAsync(query, cancellationToken);
@@ -40,6 +56,7 @@ public static class SchoolEndpoints
                 ? Results.NotFound()
                 : Results.Ok(result);
         });
+        return builder;
     }
 
 }
