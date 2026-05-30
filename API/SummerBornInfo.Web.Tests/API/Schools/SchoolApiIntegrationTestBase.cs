7d11561721082477ae0f5401edbbe2bff12890d1
JackFrostStudios <jack_reeves@live.com>
2026-05-30 20:41:08 +0100

    Reuse web integration test base for school tests

--- API/SummerBornInfo.Web.Tests/API/Schools/SchoolApiIntegrationTestBase.cs
+++ API/SummerBornInfo.Web.Tests/API/Schools/SchoolApiIntegrationTestBase.cs
@@ -2,18 +2,5 @@ namespace SummerBornInfo.Web.Tests.API.Schools;
 
 public abstract class SchoolApiIntegrationTestBase(
     IntegrationTestDatabaseServerFixture testDatabaseServerFixture,
-    ITestOutputHelper testOutputHelper) : IAsyncLifetime
-{
-    protected CustomWebApplicationFactory Factory { get; } = new(testDatabaseServerFixture, testOutputHelper);
-
-    public async ValueTask InitializeAsync()
-    {
-        await Factory.InitializeAsync();
-    }
-
-    public async ValueTask DisposeAsync()
-    {
-        await Factory.DisposeAsync();
-        GC.SuppressFinalize(this);
-    }
-}
+    ITestOutputHelper testOutputHelper)
+    : SummerBornInfo.Web.Tests.TestFramework.WebIntegrationTestBase(testDatabaseServerFixture, testOutputHelper);
