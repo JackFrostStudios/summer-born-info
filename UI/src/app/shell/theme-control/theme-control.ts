e4eaf5c2d0d328a803c56c032a7b1250468bc435
JackFrostStudios <jack_reeves@live.com>
2026-07-02 17:31:44 +0100

    Migrate theme control to shared button

--- UI/src/app/shell/theme-control/theme-control.ts
+++ UI/src/app/shell/theme-control/theme-control.ts
@@ -1,13 +1,16 @@
 import { Component, computed, inject } from '@angular/core';
+import { Button } from '../../../design-system/button/button';
 import { ThemeControlService } from './theme-control.service';
 
 @Component({
   selector: 'sbi-theme-control',
+  imports: [Button],
   templateUrl: './theme-control.html',
   styleUrl: './theme-control.scss',
 })
 export class ThemeControl {
   protected readonly colourMode = inject(ThemeControlService);
+  protected readonly buttonType = 'secondary' as const;
   protected readonly effectiveMode = this.colourMode.effectiveMode;
   protected readonly isDarkMode = computed(() => this.effectiveMode() === 'dark');
 
