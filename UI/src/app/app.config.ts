ef36b89b89d92b38512773375b56d58561d5914d
JackFrostStudios <jack_reeves@live.com>
2026-07-05 21:57:57 +0100

    Enable hydration i18n support and replay

--- UI/src/app/app.config.ts
+++ UI/src/app/app.config.ts
@@ -1,18 +1,20 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { InMemoryScrollingOptions, provideRouter, withInMemoryScrolling } from '@angular/router';
+import { provideClientHydration, withEventReplay, withI18nSupport } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 
 export const appInMemoryScrollingOptions: InMemoryScrollingOptions = {
   anchorScrolling: 'enabled',
   scrollPositionRestoration: 'enabled',
 };
 
+export const appHydrationFeatures = [withI18nSupport(), withEventReplay()];
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withInMemoryScrolling(appInMemoryScrollingOptions)),
-    provideClientHydration(),
+    provideClientHydration(...appHydrationFeatures),
   ],
 };
